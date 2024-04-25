52e86a0f5cff6ba6fa5e91e50bdecad0f94c83d1
Eldelaca <albusfgallardo@gmail.com>
2024-04-25 01:26:56 +0100

    fixed errors in code

--- Game Design Examples/controller.gd
+++ Game Design Examples/controller.gd
@@ -1,10 +1,21 @@
 extends Control
 
-var soundname
+var drum
+var piano
+var guitar
+var flute
+
+
+var audio_source
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	soundname = $"Node2D/AudioStreamPlayer"
+	
+	drum = $"Button/AudioStreamPlayer"
+	piano = $"Button2/AudioStreamPlayer2"
+	guitar = $"Button3/AudioStreamPlayer3"
+	flute = $"Button4/AudioStreamPlayer4"
+	
 	pass # Replace with function body.
 
 
@@ -14,7 +25,27 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	soundname.play()
+	drum.play()
 	
-	print("Working")
+	print("drums are playing")
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed():
+	piano.play()
+	
+	print("piano is working")
+	pass # Replace with function body.
+
+func _on_button_3_pressed():
+	guitar.play()
+	
+	print("Guitar is doing its thing")
+	pass # Replace with function body.
+
+
+func _on_button_4_pressed():
+	flute.play()
+	
+	print("flute is playing a note")
 	pass # Replace with function body.
